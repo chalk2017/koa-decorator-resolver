416b88d08cfa9a656310c07be81c600bfe63ebf7
chalk <chaoquny@cn.ibm.com>
2022-02-12 23:05:28 +0800

    update

---
+++ script/rollup.config.js
@@ -0,0 +1,30 @@
+import path from 'path'
+import resolve from 'rollup-plugin-node-resolve'; // 依赖引用插件
+import {
+    uglify
+} from 'rollup-plugin-uglify'; // 压缩代码
+const plugins = [
+    // nodejs 插件
+    resolve({
+        extensions: ['.js', '.ts']
+    }),
+    // 压缩代码
+    uglify({
+        output: {
+            comments: function(node, comment) {
+                // 不保留注释
+                return false;
+            }
+        }
+    })
+]
+
+export default [{
+    input: 'lib/resolver.js',
+    output: {
+        file: 'esm/resolver.js',
+        format: 'es',
+        name: 'resolver',
+    },
+    plugins
+}]
